0f32a5a842f40d313148a3d62acee1800057dd1b
naddamba daniellah <daniellahnaddamba@gmail.com>
2026-03-17 21:35:58 +0300

    creating Reviews model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.conf import settings
+from logbook.models import Logbook
+
+class LogReview(models.Model):
+
+    ACTION_CHOICES = [
+        ('approved', 'Approved'),
+        ('revision_requested', 'Revision Requested'),
+        ('rejected', 'Rejected')
+    ]
+
+    Logbook = models.ForeignKey(
+        Logbook,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    reviewer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='log_reviews'
+    )
+    comment = models.TextField(blank=True)
+    action = models.CharField(
+        max_length=30,
+        choices=ACTION_CHOICES
+    )
+    reviewed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review by {self.reviewer.username} on {self.Logbook}"
