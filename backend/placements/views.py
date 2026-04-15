d79deaff8af390543fb0d32753dc06052f7ceaef
Sabano Zubedah Angel <sabanozubedah@gmail.com>
2026-04-16 02:13:17 +0300

    ILES-7-views 1

--- backend/placements/views.py
+++ backend/placements/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, permissions,status
+from rest_framework.response import Response 
+from .models import InternshipPlacement
+from .serializers import PlacementSerializer, PlacementCreateSerializer
+from user.permissions import IsAdmin
+
+
+class PlacementViewset(viewsets.ModelViewSet):
+    """
+    ViewSet for InternshipPlacement.
+
+
+    Access rules:
+    -Admin:full CRUD (create,read,update,delete)
+    -Supervisor: read only(their supervised placements)
+    -Student: read only (their own plaxement)
+    """
+
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_serializer_class(self):
+        """Use lighter serializer when creating/updating."""
+        if self.action in ['create','update','partial_update']:
+            return PLacementsCreateSerializer
+        return PlacementSerializer
