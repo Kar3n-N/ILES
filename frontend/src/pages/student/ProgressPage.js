672841a5a228d1a0d5182ca19c8ab3301ad75e38
Nakibuuka Karen <k41444105@gmail.com>
2026-05-07 08:27:21 +0300

    [ILES-49]:writhing the imports on the progresspage file

--- frontend/src/pages/student/ProgressPage.js
+++ frontend/src/pages/student/ProgressPage.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import PageShell from '../components/PageShell/PageShell';
+import { PageHead, Card, Stat, Btn, Bar, Placeholder } from '../components/common/Primitives';
+import { I } from '../components/common/Icons';
+
 const SKILLS = [
     ["REST APIs", 80],
     ["Database Design", 65],
