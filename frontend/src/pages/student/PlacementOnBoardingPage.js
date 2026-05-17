de617a763c2a0dcdfe04048f3a264955483dfe60
Nakibuuka Karen <k41444105@gmail.com>
2026-05-17 17:35:17 +0300

    [ILES-62]:creating the onboardingpage function

--- frontend/src/pages/student/PlacementOnBoardingPage.js
+++ frontend/src/pages/student/PlacementOnBoardingPage.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import { PageHead, Card, Btn, Field } from '../../components/common/Primitives';
 import { I } from '../../components/common/Icons';
+
+export default function OnboardingPage() {
+  return (
+    <div className="page">
+      <PageHead
+        crumb="Onboarding · Step 1 of 2"
+        title="Set up your internship placement"
+        sub="You'll unlock your dashboard once your placement is approved."
+        actions={<><Btn kind="ghost" sm>Save draft</Btn><Btn kind="primary" sm>{I.arrow} Submit for approval</Btn></>}
+      />
