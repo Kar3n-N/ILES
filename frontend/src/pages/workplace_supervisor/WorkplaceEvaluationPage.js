411d3fdf9d56e5fb52297eb3be6bd1f566576219
Sabano Zubedah Angel <sabanozubedah@gmail.com>
2026-05-19 12:48:07 +0300

    feat(ILES-58): adding student selector

--- frontend/src/pages/workplace_supervisor/WorkplaceEvaluationPage.js
+++ frontend/src/pages/workplace_supervisor/WorkplaceEvaluationPage.js
@@ -1,1 +1,9 @@
 import { useState } from "react";
+import { PageHead, Card, Btn } from "../../component/components/common/Primitives";
+import "./WorkplaceEvaluationPage.css";
+
+const STUDENTS = [
+    { id: 1, name: "Alice Namukasa" },
+    { id: 2, name: "Brian SSemanda" },
+    { id: 3, name: "Carol Atim" },
+];
